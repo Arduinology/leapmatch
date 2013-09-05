c332e42eab7b14f237dd2f80167f459c8321f7a6
Heath Naylor <HeathNaylor@gmail.com>
2013-09-05 02:06:14 -0600

    Implement Laravel Blade Import Leap Motion JS
    Ready to begin rocking!

---
+++ app/views/hello.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel PHP Framework</title>    
+    {{ HTML::style('css/global.css') }}
+
+</head>
+<body>
+    <div class="welcome">
+        <h1>LeapMatch</h1>
+    </div>
+</body>
+{{ HTML::script('http://js.leapmotion.com/0.2.0/leap.js') }}
+</html>
